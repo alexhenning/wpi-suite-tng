e782e1442a65f1aa6249080776819b4f90a25e83
Tim Calvert <tcalvert93@gmail.com>
2013-03-17 11:47:43 -0400

    Added base for controllers. We need GUI info before we can proceed any further.

---
+++ RequirementsManagement/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/controllers/EditRequirementController.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.models.RequirementModel;
+import edu.wpi.cs.wpisuitetng.network.Network;
+import edu.wpi.cs.wpisuitetng.network.Request;
+import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
+
+/**
+ * responds to a click event to edit a requirement
+ * @author Tim
+ *
+ */
+public class EditRequirementController implements ActionListener {
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// this is still missing a lot of info, but it can't be filled in until we have some GUI info
+		
+		final Request request = Network.getInstance().makeRequest("requirementsmanager/requirementmodel",  HttpMethod.POST);
+		request.setBody(new RequirementModel().toJSON());
+		request.addObserver(new EditRequirementRequestObserver(this));
+		request.send();
+	}
+
+}
---
+++ RequirementsManagement/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/controllers/GetRequirementController.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.models.RequirementModel;
+import edu.wpi.cs.wpisuitetng.network.Network;
+import edu.wpi.cs.wpisuitetng.network.Request;
+import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
+
+/**
+ * Controller responds to a click even to retrieve Requirements from the db
+ * @author Tim
+ *
+ */
+public class GetRequirementController implements ActionListener {
+
+	public GetRequirementController() {
+		
+	}
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// this is still missing a lot of info, but it can't be filled in until we have some GUI info
+		
+		final Request request = Network.getInstance().makeRequest("requirementsmanager/requirementmodel",  HttpMethod.GET);
+		request.setBody(new RequirementModel().toJSON());
+		request.addObserver(new GetRequirementRequestObserver(this));
+		request.send();
+	}
+
+}
