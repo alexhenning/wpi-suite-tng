62d8e911e49fbdec81f37ce8c75c1f99897de6b7
Tim <tmdefreitas@wpi.edu>
2013-04-02 20:26:18 -0400

    added test file TestReleaseNumber

---
+++ RequirementsManagement/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/models/validators/TestReleaseNumberValidator.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Tim DeFreitas
+ ******************************************************************************/
+
+package edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.models.validators;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ *
+ * Description goes here
+ * @author Tim
+ *
+ */
+public class TestReleaseNumberValidator {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
