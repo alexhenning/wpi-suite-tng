220d6f8cc0b088e9a8afe9eb515f1ea7066965a7
jbmorse <jbmorse@wpi.edu>
2013-04-02 20:22:27 -0400

    Don't worry it isn't "broken" anymore

--- RequirementsManagement/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/gui/ListRequirementsPanel.java
+++ RequirementsManagement/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/gui/ListRequirementsPanel.java
@@ -42,7 +42,6 @@ import javax.swing.table.TableRowSorter;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.controllers.DB;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.controllers.RequirementsCallback;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.controllers.SingleRequirementCallback;
-import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.listeners.FilterListener;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.models.RequirementModel;
 
 @SuppressWarnings("serial")
