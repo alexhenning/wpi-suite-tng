25b1ff19e8cab2647faff1776e7216b1170e1081
Sergey Zolotykh <milk3dfx@gmail.com>
2013-04-08 20:29:54 -0400

    Added header comment

--- RequirementsManagement/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/models/validators/IterationValidatorTest.java
+++ RequirementsManagement/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/models/validators/IterationValidatorTest.java
@@ -1,6 +1,14 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Tim DeFreitas
+ ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.models.validators;
 
 import static org.junit.Assert.assertEquals;
@@ -30,7 +38,7 @@ import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.models.validators.I
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.models.validators.ValidationIssue;
 
 /**
- * @author milk3dfx
+ * @author Sergey Zolotykh (szolotykh)(milk3dfx)
  *
  */
 public class IterationValidatorTest {
