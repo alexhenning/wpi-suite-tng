ad1b1d0baa9ddbe828c34633a5ebd1d8130f91b2
Alex Henning <elcron@gmail.com>
2013-04-17 17:42:21 -0400

    Commented the report class.

--- RequirementsManagement/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/gui/reports/Report.java
+++ RequirementsManagement/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/gui/reports/Report.java
@@ -5,24 +5,46 @@ import java.util.Map;
 
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.models.RequirementModel;
 
+/**
+ * A combination of a data provider to plot and a report type to generate.
+ * 
+ * @author alex
+ */
 public class Report {
-	
+
 	ReportDataProvider dataProvider;
 	ReportType type;
-	
+
+	/**
+	 * @param dataProvider The source to get data from.
+	 * @param type The type of report to generate.
+	 */
 	public Report(ReportDataProvider dataProvider, ReportType type) {
 		this.dataProvider = dataProvider;
 		this.type = type;
 	}
 	
+	/**
+	 * A type to indicate how the report should display.
+	 */
 	public ReportType getType() {
 		return type;
 	}
-	
+
+	/**
+	 * Extracts data from a list of requirements and returns the corresponding
+	 * map.
+	 * 
+	 * @param model List of requirements.
+	 * @return some data to chart with a name and thecorresponding value
+	 */
 	public Map<Object, Integer> extractData(List<RequirementModel> model) {
 		return dataProvider.extractData(model);
 	}
-	
+
+	/**
+	 * The name of the report.
+	 */
 	@Override public String toString() {
 		return type.toString() + " of " + dataProvider.getName();
 	}
