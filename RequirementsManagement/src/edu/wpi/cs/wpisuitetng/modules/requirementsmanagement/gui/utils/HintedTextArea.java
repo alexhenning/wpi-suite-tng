25cfd615f9d0d3f0898a3e29c99d673ffdff1e0b
Visit P <vpatara@wpi.edu>
2013-04-17 20:07:40 -0400

    Improve HintedTextArea to be more flexibly extensible

--- RequirementsManagement/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/gui/utils/HintedTextArea.java
+++ RequirementsManagement/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/gui/utils/HintedTextArea.java
@@ -7,12 +7,9 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    
+ *    William Terry
+ *    vpatara
  ******************************************************************************/
-
-/**
- * @author www.how2java.com
- */
 
 package edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.gui.utils;
 
@@ -24,20 +21,27 @@ import java.awt.event.FocusEvent;
 import javax.swing.JTextArea;
 
 /**
+ * JTextArea modified to display a hinted message when empty
  *
- * TODO: DOCUMENT THIS
- * @author TODO
- *
+ * @author www.how2java.com
  */
 @SuppressWarnings("serial")
 public class HintedTextArea extends JTextArea {
 
+	private String hint;
 	Font gainFont = new Font("Tahoma", Font.PLAIN, 11);
 	Font lostFont = new Font("Tahoma", Font.ITALIC, 11);
-	
-	
+
+	/**
+	 * Constructs a hinted text area
+	 *
+	 * @param rows the number of rows for this text area
+	 * @param columns the number of columns for this text area
+	 * @param hint the hinted message when the text area is empty and inactive
+	 */
 	public HintedTextArea(int rows, int columns, final String hint) {
-		
+
+		this.hint = hint;
 		setRows(rows);
 		setColumns(columns);
 		setText(hint);
@@ -47,40 +51,61 @@ public class HintedTextArea extends JTextArea {
 		this.addFocusListener(new FocusAdapter() {
 
 			/**
-			 * what to do if focus is gained
+			 * When focus is gained, activates the text area
 			 *
 			 * @param e the event that caused focus to be gained
 			 */
 			@Override
 			public void focusGained(FocusEvent e) {
-				if (getText().equals(hint)) {
-					setText("");
-					setFont(gainFont);
-					setForeground(Color.BLACK);
-				} else {
-					setText(getText());
-					setFont(gainFont);
-				}
+				setActive(true);
 			}
 
 			/**
-			 * what to do if focus is lost
+			 * When focus is lost, deactivates the text area
 			 *
 			 * @param e the event that caused focus to be lost
 			 */
 			@Override
 			public void focusLost(FocusEvent e) {
-				if (getText().equals(hint) || getText().length() == 0) {
-					setText(hint);
-					setFont(lostFont);
-					setForeground(Color.GRAY);
-				} else {
-					setText(getText());
-					setFont(gainFont);
-					setForeground(Color.BLACK);
-				}
+				setActive(false);
 			}
 		});
+	}
 
+	/**
+	 * Sets the appearance of the object according to whether it is active or
+	 * focused
+	 *
+	 * @param active true if the focus is on the object, false otherwise
+	 */
+	private void setActive(boolean active) {
+		if(active) {
+			// Active / focused
+			setFont(gainFont);
+			if (getText().equals(hint)) {
+				setText("");
+				setForeground(Color.BLACK);
+			}
+		} else {
+			// Inactive / not focused
+			if (getText().equals(hint) || getText().length() == 0) {
+				setText(hint);
+				setFont(lostFont);
+				setForeground(Color.GRAY);
+			} else {
+				setText(getText());
+				setFont(gainFont);
+				setForeground(Color.BLACK);
+			}
+		}
+	}
+
+	/**
+	 * Determines whether the hinted text area is empty or contains the hint
+	 *
+	 * @return true if empty, false otherwise
+	 */
+	public boolean isEmpty() {
+		return getText().length() == 0 || getText().equals(hint);
 	}
 }
