870743cf0ad8efa1de3269ed9ae5154bf2be6607
Visit P <vpatara@wpi.edu>
2013-04-22 17:33:51 -0400

    Implement user-access control in the navigation toolbar view

--- RequirementsManagement/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/toolbar/NavToolbarView.java
+++ RequirementsManagement/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanagement/toolbar/NavToolbarView.java
@@ -9,6 +9,7 @@
  * Contributors:
  *    Andrew Hurle
  *    Chris Casola
+ *    vpatara
  ******************************************************************************/
 
 package edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.toolbar;
@@ -26,7 +27,10 @@ import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.actions.CreateRelea
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.actions.ShowHelpAction;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.actions.ViewIterationAction;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.actions.ViewPermissionAction;
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.db.CurrentUserPermissionManager;
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.db.SinglePermissionCallback;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.gui.utils.MainTabController;
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanagement.models.Permissions;
 
 /**
  * The Requirements Management tab's toolbar panel.
@@ -73,6 +77,7 @@ public class NavToolbarView extends ToolbarGroupView {
 		
 		permissionButton = new JButton("User Permissions");
 		permissionButton.setAction(new ViewPermissionAction(tabController));
+		permissionButton.setEnabled(false); // This button is not for every user
 		c.gridy = 2;
 		content.add(permissionButton, c);
 		
@@ -94,6 +99,27 @@ public class NavToolbarView extends ToolbarGroupView {
 		Double toolbarGroupWidth = permissionButton.getPreferredSize().getWidth() + 
 				viewReleaseNumbers.getPreferredSize().getWidth() + 40; // 40 accounts for margins between the buttons
 		setPreferredWidth(toolbarGroupWidth.intValue());
+
+		// Allow access to users with certain permission levels
+		CurrentUserPermissionManager.getInstance().addCallback(
+				new SinglePermissionCallback() {
+			@Override
+			public void failure() {} // No need to implement
+
+			@Override
+			public void callback(Permissions profile) {
+				switch (profile.getPermissionLevel()) {
+				case ADMIN:
+					// Administrator can do everything
+					permissionButton.setEnabled(true);
+					break;
+
+				default:
+					// "Update" and "none" can't do anything
+					break;
+				}
+			}
+		});
 	}
 
 	class ShowReportsAction extends AbstractAction {
